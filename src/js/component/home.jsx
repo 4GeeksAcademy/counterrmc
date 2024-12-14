e44800503d0acae0e44de5f6c27b579a78965a21
Ruben <167398241+rumacon@users.noreply.github.com>
2024-12-14 04:21:20 +0000

    rey del hardcoreo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+function reloj() {
+	const [timer, setTimer] = React.useState(99);
+	const id = React.useRef(null);
+	
+	React.useEffect(() => {
+	  id.current = window.setInterval(() => {
+		setTimer((time) => time - 1);
+	  }, 1000);
+	  return () => clear();
+	}, []);
+  
+	React.useEffect(() => {
+	  if (timer === 0) {
+		clear();
+	  }
+	}, [timer]);
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="reloj">
+		<img className="img" src="https://media.newyorker.com/photos/62e85962fdcd53b7bf03b3b3/3:2/w_3000,h_2000,c_limit/gettyimages-681898.jpg"/>
+		<div className="position-absolute top-50 start-50 translate-middle badge text-bg-danger text-wrap fs-1">
+		<p>00{timer} </p>
 		</div>
+	  </div>
 	);
-};
-
-export default Home;
+  }
+		
+  export default reloj
