256476e269dbcc0d3d1bf6f0a4cd25a7773d645d
Ruben <167398241+rumacon@users.noreply.github.com>
2024-12-19 23:50:12 +0000

    improve

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -38,31 +38,66 @@ function reloj() {
 	let counter1 = count.toString().padStart(4, 0);
 	let counter2 = count2.toString().padStart(4, 0);
 
-	
+
 
 	return (
-	<div className="row">
-		<div className="card text-center col-6" style={{width:'400px'}}>
-			
-			<img src="https://cdn-images-3.listennotes.com/podcasts/se%C3%B1ores-dejo-todo/t2-e14-el-tano-pasman-pEcqXrynbPC-BMXwpCeJDEr.1400x1400.jpg"/>
-			<div className="card-img-overlay text-warning" style={{position: "absolute",
-			top: "50px", left:"45px", width:"120px", height:"105px" }}>
-				<h2 className="position-absolute top-50 start-50 translate-middle">{counter1}</h2>
+
+
+		<>
+
+			<div className="card text-center col-6" style={{ width: '400px' }}>
+
+				<img src="https://cdn-images-3.listennotes.com/podcasts/se%C3%B1ores-dejo-todo/t2-e14-el-tano-pasman-pEcqXrynbPC-BMXwpCeJDEr.1400x1400.jpg" />
+				<div className="card-img-overlay text-warning" style={{
+					position: "absolute",
+					top: "50px", left: "45px", width: "120px", height: "105px"
+				}}>
+					<h2 className="position-absolute top-50 start-50 translate-middle">
+						{counter1}</h2>
+				</div>
+				<h1>No te detengas Carajo!!!</h1>
 			</div>
-			<h1>No te detengas Carajo!!!</h1>
-		
-		</div>
-		<div className="card text-center col-6" style={{width:'400px'}}>
-			
-			<img src="https://www.agencianova.com/data/fotos2/bbx_307685074_tano.png"/>
-			<div className="card-img-overlay text-danger" style={{position: "absolute",
-			top: "15px", left:"20px", width:"120px", height:"105px" }}>
-				<h2 className="position-absolute top-50 start-50 translate-middle">{counter2}</h2>
+
+
+
+
+			<button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+				Haz enojar a Tano!
+			</button>
+
+
+			<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+							<div className="card text-center col-6" style={{ width: '400px' }}>
+
+
+								<img src="https://www.agencianova.com/data/fotos2/bbx_307685074_tano.png" />
+								<div className="card-img-overlay text-danger" style={{
+									position: "absolute",
+									top: "15px", left: "20px", width: "120px", height: "105px"
+								}}>
+									<h2 className="position-absolute top-50 start-50 translate-middle">{counter2}</h2>
+								</div>
+								<h1>LPQM Pariooo !!!</h1>
+
+							</div>
+
+
+
+
+						</div>
+						
+					</div>
+				</div>
 			</div>
-			<h1>LPQM Pariooo !!!</h1>
-		
-		</div>
-	</div>
+
+		</>
 	);
 }
 
